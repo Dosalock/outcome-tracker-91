a138594cdd853965d8ea19840e4323f1c4c0dec3
Johan Karlsson <59482008+Dosalock@users.noreply.github.com>
2025-09-01 15:21:11 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/outcome-tracker/',
+  base: '/outcome-tracker-91/',
   server: {
     host: "::",
     port: 8080,
